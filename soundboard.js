222b2779b6a3c364a2df0effc1d3862ebb5b5e6b
Armand DiLisio <armand_dilisio@Armands-MacBook-Pro.local>
2020-11-13 17:00:13 -0500

    soundboard

---
+++ soundboard.js
@@ -0,0 +1,15 @@
+var buttonElement = document.getElementById('button-one')
+var audioOne = document.getElementById('one')
+buttonElement.addEventListener('click', function(){
+   audioOne.play()
+})
+var buttonElement = document.getElementById('button-two')
+var audioTwo = document.getElementById('two')
+buttonElement.addEventListener('click', function(){
+   audioTwo.play()
+})
+var buttonElement = document.getElementById('button-three')
+var audioThree = document.getElementById('three')
+buttonElement.addEventListener('click', function(){
+   audioThree.play()
+})
